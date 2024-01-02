32e9292b6001ebce64b3a44d9c97f6fbfb272e03
samratmahmud <akashrahman.me@gmail.com>
2024-01-02 10:14:37 +0600

    water ripple create

--- src/app/home/Header.tsx
+++ src/app/home/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import React, {useEffect} from "react";
-// import WaterWave from "react-water-wave";
+import WaterWave from "react-water-wave";
 
 const socialItems = [
   {
@@ -20,42 +20,45 @@ const socialItems = [
 
 function Header() {
   return (
-    <section
-      id="header"
-      className="py-28 grid place-content-center h-screen bg-slate-900"
-    >
-      {/* <WaterWave imageUrl={""} perturbance={0.03}>
-        {() => ( */}
-      <div className="container">
-        <div className="mb-5">
-          <img
-            className="w-48 h-48 rounded-full border-[6px] border-slate-600/10 m-auto"
-            src="/images/hero-img.jpg"
-            alt=""
-          />
-        </div>
-        <h1 className="text-7xl font-semibold text-gray-100 text-center mb-5">
-          Hi, I am <span className="text-primary">Jhon Doe</span>
-        </h1>
-        <p className="text-center mb-5">
-          I am a frontend web developer. I can provide clean code and pixel
-          perfect design. <br /> I also make website more & more interactive
-          with web animations.
-        </p>
-        <div className="flex justify-center gap-5">
-          {socialItems.map(({icon, link}, index) => (
-            <Link href={link} key={index} className="">
-              <img
-                className="w-11 h-11 border-2 border-slate-600/25 hover:border-slate-400 rounded-full p-2.5 opacity-70 hover:opacity-100 duration-300"
-                src={icon}
-                alt=""
-              />
-            </Link>
-          ))}
-        </div>
-      </div>
-      {/* )}
-      </WaterWave> */}
+    <section>
+      <WaterWave
+        imageUrl={"/images/Screenshot 2024-01-02 094409.png"}
+        perturbance={0.03}
+        className="bg-cover bg-center"
+      >
+        {() => (
+          <div id="header" className="py-28 grid place-content-center h-screen">
+            <div className="container">
+              <div className="mb-5">
+                <img
+                  className="w-48 h-48 rounded-full border-[6px] border-slate-600/10 m-auto"
+                  src="/images/hero-img.jpg"
+                  alt=""
+                />
+              </div>
+              <h1 className="text-7xl font-semibold text-gray-100 text-center mb-5">
+                Hi, I am <span className="text-primary">Jhon Doe</span>
+              </h1>
+              <p className="text-center text-sm mb-5">
+                I am a frontend web developer. I can provide clean code and
+                pixel perfect design. <br /> I also make website more & more
+                interactive with web animations.
+              </p>
+              <div className="flex justify-center gap-5">
+                {socialItems.map(({icon, link}, index) => (
+                  <Link href={link} key={index} className="">
+                    <img
+                      className="w-11 h-11 border-2 border-slate-600/25 hover:border-slate-400 rounded-full p-2.5 opacity-70 hover:opacity-100 duration-300"
+                      src={icon}
+                      alt=""
+                    />
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </WaterWave>
     </section>
   );
 }
--- src/components/global/navbar/Navbar.tsx
+++ src/components/global/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Link from "next/link";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import NavItem from "./NavItem";
 import {usePathname} from "next/navigation";
+import {strict} from "assert";
+import {stringify} from "querystring";
 
 const navItems = [
   {
@@ -32,20 +34,42 @@ const navItems = [
 ];
 
 function Navbar() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handelScroll = () => {
+      if (window.scrollY >= 10) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    handelScroll();
+
+    window.addEventListener("scroll", () => {
+      handelScroll();
+    });
+  }, []);
+
   return (
-    <nav className="bg-gray-900 opacity-90 py-3.5 fixed left-0 right-0 z-[1020]">
+    <nav
+      className={`opacity-90 py-3.5 fixed left-0 right-0 z-[1020] duration-500 ${
+        isVisible ? "bg-gray-900" : "bg-transparent"
+      }`}
+    >
       <div className="container">
         <div className="flex gap-3 items-center justify-between">
           <Link href="/">
             <img src="/images/logo.png" alt="" />
           </Link>
-          <div className="flex items-center gap-11">
+          <div className="flex items-center gap-10">
             <div className="flex gap-10">
               {navItems.map(({title, link}, index) => (
                 <NavItem key={index} title={title} link={link} />
               ))}
             </div>
-            <div className="border border-gray-100 h-5" />
+            <div className="border border-gray-100 h-3" />
             <div className="flex items-center gap-4">
               <img
                 className="w-5 h-5"
